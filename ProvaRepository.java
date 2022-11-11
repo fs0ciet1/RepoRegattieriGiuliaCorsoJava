c8e3150b639d1635922e1c2333bb34a81f0cb6e5
fs0ciet1 <117976291+fs0ciet1@users.noreply.github.com>
2022-11-11 11:21:52 +0100

    Add files via upload

---
+++ ProvaRepository.java
@@ -0,0 +1,52 @@
+class ProvaRepository
+{
+    public static void main(String args[]) 
+    {
+        //diachiaro variabili
+        double uno;
+        double due;
+        double tre;
+        double somma;
+        double risultato;
+        boolean ToF1;
+        boolean ToF2;
+        boolean EVVIVAAA;
+
+
+
+
+        //variabili  da utilizzare in seguito
+        
+
+
+         //inizializzo variabili
+         uno='b';
+         due=123.34;
+         tre=1;
+         ToF1= true;
+         ToF2=false;
+
+
+
+         //qui faccio operazioni 
+         somma=uno+due+tre; 
+         risultato= somma/3;
+         EVVIVAAA = (ToF1 == !ToF2);
+
+
+         //qui stampo
+         System.out.println(somma);
+         System.out.println(risultato);
+
+
+        //condizioni per stampare EVVIVA
+        System.out.println(ToF1 == !(ToF2));
+
+
+
+
+         
+        
+        
+    }
+}
