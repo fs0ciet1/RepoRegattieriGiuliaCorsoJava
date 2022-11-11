53afe84e162ae091287811cd5affc11cf663a8c4
fs0ciet1 <117976291+fs0ciet1@users.noreply.github.com>
2022-11-11 18:24:03 +0100

    Add files via upload

---
+++ VariabiliMatematiche.java
@@ -0,0 +1,39 @@
+class VariabiliMatematiche
+{
+    public static void main(String args[]) 
+    {
+        //diachiaro variabili
+        double uno;
+        double due;
+        double tre;
+        double quattro;
+        double cinque;
+        double somma;
+        double divisione;
+        double divisione2;
+        
+        //inizializzo variabili
+        uno = 1;
+        due = 12;
+        tre= 14;
+        quattro = 122;
+        cinque =  12.456;
+
+        //qui faccio operazioni
+        somma= uno+due+tre+quattro+cinque;
+        divisione = somma/2;
+        divisione2 = divisione/5;
+        
+        //qui stampo
+        System.out.println(somma);
+        System.out.println(divisione);
+        System.out.println(divisione2);
+        System.out.println(divisione2);
+
+
+
+        
+
+
+    }
+}
