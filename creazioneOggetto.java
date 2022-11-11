2093b76540456cafd4c1132ea5b443c99c5b1d9c
fs0ciet1 <117976291+fs0ciet1@users.noreply.github.com>
2022-11-11 14:58:06 +0100

    Add files via upload

---
+++ creazioneOggetto.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class creazioneOggetto
+{
+    public static void main(String []args) 
+    {
+        Scanner Myobj= new Scanner (System.in); //creazione di un oggetto di tipo scanner
+        //Scanner objInteri = new Scanner(System.in); //per usare gli interi
+        System.out. println ("Metti il tuo Nick: ");
+
+        String nome=Myobj.nextLine(); //read user input di tipo stringa
+        System.out.println();
+        System.out.println("Il tuo Nick è" + nome); //output per user input
+
+        
+
+    }
+}
