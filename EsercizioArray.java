4d022a382cbcfa0547d3c02a48b4dfd645c1497d
Giulia Regattieri <117976291+fs0ciet1@users.noreply.github.com>
2022-11-15 23:31:08 +0100

    Add files via upload

---
+++ EsercizioArray.java
@@ -0,0 +1,27 @@
+class esercizioArray
+{
+    public static void main(String args[]) 
+    /*{
+        String [] cani = { "Bau", "BAU2", "FRANK", "Madlin"};
+        System.out.println(cani[0]); 
+    }*/
+
+
+    //stampo l'array in posizione 1
+   /* {
+        String [] cani = { "Bau", "BAU2", "FRANK", "Madlin"};
+        cani[1]="Sammy";
+        System.out.println(cani[1]); 
+    }*/
+
+
+
+    //stampo quanti elementi ha un array con proprietà length
+    {
+        String [] cani = { "Bau", "BAU2", "FRANK", "Madlin"};
+        cani[1]="Sammy";
+        System.out.println(cani.length); 
+    }
+
+
+}
